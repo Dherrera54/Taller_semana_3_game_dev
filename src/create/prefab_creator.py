7af1b63c088f4236707545fb867cb5915e332004
Dario_herrera <d.herrerag@uniandes.edu.co>
2023-04-18 20:55:54 -0500

    added create_enemy_hunter

--- src/create/prefab_creator.py
+++ src/create/prefab_creator.py
@@ -47,6 +47,13 @@ def create_enemy_square(world: esper.World, pos: pygame.Vector2, enemy_info: dic
     enemy_entity = create_sprite(world,  pos, velocity, enemy_surface)
     world.add_component(enemy_entity, CTagEnemy())
 
+def create_enemy_hunter(world: esper.World, pos: pygame.Vector2, enemy_info: dict):
+    enemy_surface = pygame.image.load(enemy_info["image"]).convert_alpha()    
+    velocity = pygame.Vector2(0,0)
+    enemy_entity = create_sprite(world,  pos, velocity, enemy_surface)
+    world.add_component(enemy_entity, CTagEnemy())
+    world.add_component(enemy_entity, CAnimation(enemy_info["animations"]))
+
 
 def create_player_square(world: esper.World, player_info: dict, player_lvl_info: dict) -> int:
     player_sprite = pygame.image.load(player_info["image"]).convert_alpha()
--- src/engine/game_engine.py
+++ src/engine/game_engine.py
@@ -57,6 +57,8 @@ class GameEngine:
             self.player_cfg = json.load(player_file)
         with open("assets/cfg/bullet.json") as bullet_file:
             self.bullet_cfg = json.load(bullet_file)
+        with open("assets/cfg/explosion.json") as explosion_file:
+            self.explosion_cfg = json.load(explosion_file)
 
     def run(self) -> None:
         self._create()
